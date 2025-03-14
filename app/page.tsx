c692643490b29a1b468f61fdfda73a386b1e3f10
Ayush Kumar Pandey <pandeysmokey@gmail.com>
2025-03-14 23:28:22 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import { GitHubStats } from "@/components/sections/GitHubStats";
 import Navbar from "@/components/sections/Navbar";
 import Contact from "@/components/sections/Contact";
 import Footer from "@/components/sections/Footer";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
